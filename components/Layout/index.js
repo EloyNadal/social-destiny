e0676bcbfdcb6d139104f4a1351fd0ac8d5f46d5
Eloy Nadal Escobar <bydoser@gmail.com>
2022-01-14 23:04:45 +0100

    modificar diseno inicial

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,19 +1,19 @@
 import Avatar from 'components/Avatar';
 import Nav from 'components/Nav';
-import useUser from 'hooks/useUser';
+import { useSession } from 'next-auth/react';
 import styles from './Layout.module.css';
 
 export default function Layout({ children }) {
 
-    const user = useUser();
+    const { data: session } = useSession()
 
     return (
         <main>
 
             <header className={styles.header}>
                 {
-                    user && user.avatar &&
-                    <Avatar src={user.avatar} alt={user.userName} sizeW={40} sizeH={40} />
+                    session && 
+                    <Avatar src={session.user.image} alt={session.user.name} sizeW={40} sizeH={40} />
                 }
                 <h2>Inicio</h2>
             </header>
